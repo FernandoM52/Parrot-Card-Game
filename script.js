992f283f18ddd81515f6669d8c408401be94761f
Fernando Augusto <fernandomartins.contato@outlook.com.br>
2023-01-17 12:47:51 -0300

    Versão com as funções comentadas

--- script.js
+++ script.js
@@ -5,17 +5,19 @@ enquanto o numero não atender as condições, o prompt deve ser repetido*/
 
 function numeroCartas() {
     while (numCartas % 2 !== 0 || numCartas < 4 || numCartas > 14) {
-        numCartas = parseInt(prompt("Quantas cartas quer jogar?")); {
+        numCartas = parseInt(prompt("Digite um número par de 4 a 14 para selecionar com quantas cartas você quer jogar.")); {
 
             if (numCartas % 2 !== 0 || numCartas < 4 || numCartas > 14) {
                 alert("Número inválido, digite novamente.")
+            } else {
+                alert(`Modo de jogo selecionado: ${numCartas} cartas`);
             }
         }
     }
 }
-numeroCartas(); //executa a condição do numero de cartas
+numeroCartas();
 
-//Declarando as imgs dentro do js
+// Declarando as imgs dentro do js
 let img1 = `<img data-test="face-up-image" src="./imgs/bobrossparrot.gif">`;
 let img2 = `<img data-test="face-up-image" src="./imgs/explodyparrot.gif">`;
 let img3 = `<img data-test="face-up-image" src="./imgs/fiestaparrot.gif">`;
@@ -40,13 +42,13 @@ function embaralhar() {
     return Math.random() - 0.5;
 }
 
-//Distribuir cartas no tabuleiro: loop que manipula o DOM para executar a criação das cartas através do banco de cartas do jogo
+// Distribuição das cartas no tabuleiro: loop que manipula o DOM para ciar as cartas dinamicamente através do banco de cartas do jogo
 function distribuirCartas() {
     const ul = document.querySelector('ul');
 
     for (let j = 0; j < numCartas; j++) {
         const li = document.createElement('li');
-        const createCard = document.createElement('div');
+        const createCard = document.createElement("div");
         createCard.classList.add("card");
         createCard.setAttribute("data-test", "card");
         createCard.setAttribute("onclick", "virarCarta(this)");
@@ -71,7 +73,7 @@ let firstClick = '';
 let secondClick = '';
 let jogadas = 0;
 
-//Virar as cartas ao clicar
+// Função que gira as cartas ao clicar e armanzena as duas cartas clicadas em variaveis diferentes para que sejam comparadas posteriormente
 function virarCarta(cartaClicada) {
     if (cartaClicada.classList.contains("virar-carta")) {
         return;
@@ -84,12 +86,12 @@ function virarCarta(cartaClicada) {
         cartaClicada.classList.add("virar-carta");
         secondClick = cartaClicada;
 
-        setTimeout(validaCartas, 1000)
-
+        setTimeout(validaCartas, 1000) // Executa a verificação das cartas 1s após virar a segunda carta
     }
     jogadas++;
 }
 
+// Função que confere se as cartas clicadas são iguas e reseta as variáveis das cartas clicadas após a verificação
 function validaCartas() {
     const primeiraCarta = firstClick.getAttribute("data-parrot");
     const segundaCarta = secondClick.getAttribute("data-parrot");
@@ -100,7 +102,7 @@ function validaCartas() {
         firstClick = '';
         secondClick = '';
 
-        setTimeout(fimDoJogo, 500);
+        setTimeout(fimDoJogo, 500); // Executa a função fim de jogo quando todas as cartas estiverem viradas para cima
 
     } else {
         firstClick.classList.remove('virar-carta');
@@ -110,9 +112,10 @@ function validaCartas() {
     }
 }
 
+// Função que verifica se todas as cartas viradas possuem a classe "parEncontrado", se sim, o jogo terminará
 function fimDoJogo() {
     const paresEncontrados = document.querySelectorAll('.parEncontrado');
     if (paresEncontrados.length === numCartas) {
-        alert(`Você ganhou em ${jogadas} jogadas!`)
+        alert(`Você ganhou em ${jogadas} jogadas!`);
     }
 }
